dde003426abbf9bd484e90e2434088c65a9aa12e
PC <you@example.com>
2021-04-22 19:56:22 +0545

    my music project

---
+++ src/nav/Navn.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Navn = () => {
+    return (
+        <div>
+            <h1>Music</h1>
+            <button>
+                Library
+            </button>
+        </div>
+    )
+}
