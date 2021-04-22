094035061c2e10f3a057d34d5d8c746509f97275
PC <you@example.com>
2021-04-22 20:11:00 +0545

    Updates

---
+++ precache-manifest.53a91575dc4208d9a96f1a10a79b8039.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d8582e36ff815015765ad6d7812cef7",
+    "url": "/.github.io/music/index.html"
+  },
+  {
+    "revision": "f1b087c09398fdb3276b",
+    "url": "/.github.io/music/static/css/main.10d19844.chunk.css"
+  },
+  {
+    "revision": "4b83b4c2101cb29f3f9a",
+    "url": "/.github.io/music/static/js/2.ace8b4a0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/.github.io/music/static/js/2.ace8b4a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1b087c09398fdb3276b",
+    "url": "/.github.io/music/static/js/main.533a1ef1.chunk.js"
+  },
+  {
+    "revision": "8b369e61c26c6c8cdcef",
+    "url": "/.github.io/music/static/js/runtime-main.efd5be4b.js"
+  },
+  {
+    "revision": "1b25d23a2b6df14c7c65987d9b4e3112",
+    "url": "/.github.io/music/static/media/1.1b25d23a.jpg"
+  },
+  {
+    "revision": "38e629faf69744c8074b99da97a925f8",
+    "url": "/.github.io/music/static/media/2.38e629fa.jpg"
+  },
+  {
+    "revision": "962ffd23111028a97b4533a52b5795cf",
+    "url": "/.github.io/music/static/media/3.962ffd23.jpg"
+  },
+  {
+    "revision": "01ff1a748f108528b3456619a025e5e1",
+    "url": "/.github.io/music/static/media/4.01ff1a74.jpg"
+  },
+  {
+    "revision": "a3851c0fe6d08f5f8d7dee47f7d57b11",
+    "url": "/.github.io/music/static/media/5.a3851c0f.jpg"
+  },
+  {
+    "revision": "5cacdb0c24f46cc980b50b6156f0e48c",
+    "url": "/.github.io/music/static/media/6.5cacdb0c.jpg"
+  },
+  {
+    "revision": "f2011b9af0a27fe0f747bdf96a10d9b6",
+    "url": "/.github.io/music/static/media/7.f2011b9a.jpg"
+  },
+  {
+    "revision": "d50462b24e44250d6e7e4b65f57097c1",
+    "url": "/.github.io/music/static/media/Alan Walker - Sing Me To Sleep.d50462b2.webm"
+  },
+  {
+    "revision": "c60d0960932f71cb85a09a9b846c0861",
+    "url": "/.github.io/music/static/media/Alan Walker - The Spectre.c60d0960.webm"
+  },
+  {
+    "revision": "cb2963e3d63f9b88074ca612978a985b",
+    "url": "/.github.io/music/static/media/Alan Walker -Alone.cb2963e3.webm"
+  },
+  {
+    "revision": "be4973e3aac0fd335ab516f1df0e3678",
+    "url": "/.github.io/music/static/media/Ed Sheeran - Beautiful People.be4973e3.webm"
+  },
+  {
+    "revision": "5866eb87139af33d2b0d8dbf65833b05",
+    "url": "/.github.io/music/static/media/Ed Sheeran - Perfect.5866eb87.webm"
+  },
+  {
+    "revision": "916d24c886542e8b091734bb8c7a2d2d",
+    "url": "/.github.io/music/static/media/Eminem - Lucky You.916d24c8.webm"
+  },
+  {
+    "revision": "4cad9a3705e832cd81934a4b14a93b71",
+    "url": "/.github.io/music/static/media/I Don't Care.4cad9a37.webm"
+  },
+  {
+    "revision": "8419a69da068e8a3e5f0c4b35ede25f4",
+    "url": "/.github.io/music/static/media/Imagine Dragons - Natural.8419a69d.webm"
+  },
+  {
+    "revision": "b7ec58955e5265bfdd6c2f83da2bb602",
+    "url": "/.github.io/music/static/media/Imagine Dragons - Whatever It Takes.b7ec5895.webm"
+  },
+  {
+    "revision": "b912b3c96f2a047ecdc0e7712d2a2515",
+    "url": "/.github.io/music/static/media/Marshmello - Alone.b912b3c9.webm"
+  },
+  {
+    "revision": "f5559abbcacb94b3458f4edd8e45b9d1",
+    "url": "/.github.io/music/static/media/Marshmello ft. Bastille - Happier.f5559abb.webm"
+  }
+]);
